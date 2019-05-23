6c2c8e1c7eaef04b2999fe1214c85839cfcd314e
Felix Wehnert <flashspys@gmail.com>
2019-05-23 14:27:05 +0200

    Added a VC for the TireService

---
+++ e.GO Service/View/DetailView/TireServiceViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TireServiceViewController.swift
+//  e.GO Service
+//
+//  Created by Felix Wehnert on 23.05.19.
+//  Copyright © 2019 Jonas Schlabertz. All rights reserved.
+//
+
+import UIKit
+
+class TireServiceViewController: UIViewController {
+    
+    @IBOutlet weak var bottomLeftLabel: UILabel!
+    @IBOutlet weak var bottomRightLabel: UILabel!
+    @IBOutlet weak var topRightLabel: UILabel!
+    @IBOutlet weak var topLeftLabel: UILabel!
+    
+    var vehicleSignalList: VehicleSignalList?
+    
+    override func viewDidLoad() {
+        if let signalList = vehicleSignalList {
+            bottomLeftLabel.text = String(signalList.tirePressureBackLeft)
+            bottomRightLabel.text = String(signalList.tirePressureBackRight)
+            topRightLabel.text = String(signalList.tirePressureFrontRight)
+            topLeftLabel.text = String(signalList.tirePressureFrontLeft)
+        }
+    }
+    
+}
